62a3b320132d0dced9fc1038400bbffc889df7ca
WangShanren-uestc <201522070315@std.uestc.edu.cn>
2017-05-17 08:58:14 +0800

    code

---
+++ HUAEWI_CODE_CRAFT_2017/SDK-gcc/build/CMakeFiles/cdn.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/w/huwei_2017_4.0/SDK-gcc/cdn/cdn.cpp" "/home/w/huwei_2017_4.0/SDK-gcc/build/CMakeFiles/cdn.dir/cdn.cpp.o"
+  "/home/w/huwei_2017_4.0/SDK-gcc/cdn/deploy.cpp" "/home/w/huwei_2017_4.0/SDK-gcc/build/CMakeFiles/cdn.dir/deploy.cpp.o"
+  "/home/w/huwei_2017_4.0/SDK-gcc/cdn/io.cpp" "/home/w/huwei_2017_4.0/SDK-gcc/build/CMakeFiles/cdn.dir/io.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/w/huwei_2017_4.0/SDK-gcc/cdn/lib"
+  "/home/w/huwei_2017_4.0/SDK-gcc/cdn"
+  "/usr/include/python2.7"
+  "/usr/include/x86_64-linux-gnu/python2.7"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ HUAEWI_CODE_CRAFT_2017/SDK-gcc/build/CMakeFiles/cdn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cdn.dir/deploy.cpp.o"
+  "CMakeFiles/cdn.dir/cdn.cpp.o"
+  "CMakeFiles/cdn.dir/io.cpp.o"
+  "/home/w/huwei_2017_4.0/SDK-gcc/bin/cdn.pdb"
+  "/home/w/huwei_2017_4.0/SDK-gcc/bin/cdn"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cdn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
